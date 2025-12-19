8ec45986974c9e14a33f16aa02f0c074e37cd725
Vadzim Ivanitski <urlich.livonian@gmail.com>
2025-12-19 16:03:30 +0400

    Add sample notebook for Natural Language API with entity and syntax analysis

---
+++ gcp-vi-mlops/00-introduction/nl-and-prompts-with-vertex-aai/nl-api.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Entity and Sentiment Analysis with the Natural Language API
+
+API_KEY='blablabla'
+
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json > ./result.json
+
+curl "https://language.googleapis.com/v1/documents:analyzeSentiment?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request_1.json
+
+curl "https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request_2.json
+
+curl "https://language.googleapis.com/v1/documents:analyzeSyntax?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request_3.json
